b14ca11b8169300d0adb3fd941ec6c1ceefa1913
Steven Bryant <stevelad_83@yahoo.com>
2023-01-17 19:12:38 -0500

    removed console logs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,6 @@ import BlogCard from '../BlogCard/BlogCard.js';
 //use custom hook, map through blog list and render Blogcard component for each
 export default function Main() {
   const blogs = useBlogs();
-  console.log('blogs', blogs);
   return (
     <main>
       {blogs.map((blog) => (
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,6 @@
 import { client, checkError } from './client.js';
 
 export async function getBlogs() {
-  const response = await client.from('blogs').select('*');
+  const response = await client.from('blogs').select('*, authors(name)');
   return checkError(response);
 }
